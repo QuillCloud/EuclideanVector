58b26b24198ddbeee114b80ea15699454b2d995e
awszwc <xlb25@126.com>
2017-08-22 16:52:39 +1000

    complete 1.0

--- EuclideanVector.cpp
+++ EuclideanVector.cpp
@@ -7,8 +7,11 @@
 using evec::EuclideanVector;
 
 //Constructor
-EuclideanVector::EuclideanVector(const unsigned int n) : num_dim{n}, mag_vec{new double[n]} {}
-
+EuclideanVector::EuclideanVector(const unsigned int n) : num_dim{n}, mag_vec{new double[n]} {
+    for (auto i = 0; i < num_dim; ++i) {
+        mag_vec[i] = 0;
+    }
+}
 EuclideanVector::EuclideanVector() : EuclideanVector(1) {}
 
 EuclideanVector::EuclideanVector(const unsigned int n,const double d) : num_dim{n} {
@@ -90,26 +93,162 @@ unsigned int EuclideanVector::getNumDimensions() const {
     return num_dim;
 }
 
-double EuclideanVector::get(unsigned int i) const {
-    if (num_dim >= i)
-        return mag_vec[i];
-    else {
-        std::cout << "Error: out of range" << std::endl;
-        return NULL;
-    }
+double EuclideanVector::get(int index) const {
+    assert((index >= 0) && (index < num_dim));
+    return mag_vec[index];
 }
 
 double EuclideanVector::getEuclideanNorm() const{
-    int sum = 0;
+    double sum = 0;
     for (auto i = 0; i < num_dim; ++i) {
         sum += pow(mag_vec[i], 2);
-        std::cout << sum << std::endl;
     }
     return sqrt(sum);
 }
 
+EuclideanVector EuclideanVector::createUnitVector() const{
+    std::vector<double> v;
+    double en = getEuclideanNorm();
+    for (auto i = 0; i < num_dim; ++i) {
+        v.push_back(mag_vec[i] / en);
+    }
+    return EuclideanVector(v.begin(), v.end());
+}
 
+//set
+double& EuclideanVector::operator[](int index) {
+    assert((index >= 0) && (index < num_dim));
+    return mag_vec[index];
+}
 
+double EuclideanVector::operator[](int index) const {
+    assert((index >= 0) && (index < num_dim));
+    return mag_vec[index];
+}
+
+EuclideanVector& EuclideanVector::operator+=(const EuclideanVector& ev) {
+    assert(num_dim == ev.num_dim);
+    for (auto i = 0; i < num_dim; ++i) {
+        mag_vec[i] += ev.mag_vec[i];
+    }
+    return *this;
+}
+
+EuclideanVector& EuclideanVector::operator-=(const EuclideanVector& ev) {
+    assert(num_dim == ev.num_dim);
+    for (auto i = 0; i < num_dim; ++i) {
+        mag_vec[i] -= ev.mag_vec[i];
+    }
+    return *this;
+}
+
+EuclideanVector& EuclideanVector::operator*=(double d) {
+    for (auto i = 0; i < num_dim; ++i) {
+        mag_vec[i] *= d;
+    }
+    return *this;
+}
+
+EuclideanVector& EuclideanVector::operator/=(double d) {
+    for (auto i = 0; i < num_dim; ++i) {
+        mag_vec[i] /= d;
+    }
+    return *this;
+}
+
+EuclideanVector::operator std::vector<double>() const {
+    std::vector<double> v;
+    for (auto i = 0; i < num_dim; ++i) {
+        v.push_back(mag_vec[i]);
+    }
+    return v;
+}
+
+EuclideanVector::operator std::list<double>() const {
+    std::list<double> l;
+    for (auto i = 0; i < num_dim; ++i) {
+        l.push_back(mag_vec[i]);
+    }
+    return l;
+}
+
+bool evec::operator==(const EuclideanVector& ev1, const EuclideanVector& ev2) {
+    if (ev1.num_dim != ev2.num_dim) {
+        return false;
+    }
+    for (auto i = 0; i < ev1.num_dim; ++i) {
+        if (ev1.mag_vec[i] != ev2.mag_vec[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool evec::operator!=(const EuclideanVector& ev1, const EuclideanVector& ev2) {
+    return !(ev1 == ev2);
+}
+
+EuclideanVector evec::operator+(const EuclideanVector& ev1, const EuclideanVector& ev2) {
+    assert(ev1.num_dim == ev2.num_dim);
+    std::vector<double> v;
+    for (auto i = 0; i < ev1.num_dim; ++i) {
+        v.push_back(ev1.mag_vec[i] + ev2.mag_vec[i]);
+    }
+    return EuclideanVector(v.begin(), v.end());
+}
+
+EuclideanVector evec::operator-(const EuclideanVector& ev1, const EuclideanVector& ev2) {
+    assert(ev1.num_dim == ev2.num_dim);
+    std::vector<double> v;
+    for (auto i = 0; i < ev1.num_dim; ++i) {
+        v.push_back(ev1.mag_vec[i] - ev2.mag_vec[i]);
+    }
+    return EuclideanVector(v.begin(), v.end());
+}
+
+double evec::operator*(const EuclideanVector& ev1, const EuclideanVector& ev2) {
+    assert(ev1.num_dim == ev2.num_dim);
+    double result = 0;
+    for (auto i = 0; i < ev1.num_dim; ++i) {
+        result += ev1.mag_vec[i] * ev2.mag_vec[i];
+    }
+    return result;
+}
+
+EuclideanVector evec::operator*(const EuclideanVector& ev, double d_num) {
+    std::vector<double> v;
+    for (auto i = 0; i < ev.num_dim; ++i) {
+        v.push_back(ev.mag_vec[i] * d_num);
+    }
+    return EuclideanVector(v.begin(), v.end());
+}
+
+EuclideanVector evec::operator*(double d_num, const EuclideanVector& ev) {
+    std::vector<double> v;
+    for (auto i = 0; i < ev.num_dim; ++i) {
+        v.push_back(ev.mag_vec[i] * d_num);
+    }
+    return EuclideanVector(v.begin(), v.end());
+}
+
+EuclideanVector evec::operator/(const EuclideanVector &ev, double d_num) {
+    std::vector<double> v;
+    for (auto i = 0; i < ev.num_dim; ++i) {
+        v.push_back(ev.mag_vec[i] / d_num);
+    }
+    return EuclideanVector(v.begin(), v.end());
+}
+
+std::ostream& evec::operator<<(std::ostream &os, const EuclideanVector &ev) {
+    os << "[";
+    for (auto i = 0; i < ev.num_dim; ++i) {
+        os << ev.mag_vec[i];
+        if (i != ev.num_dim - 1)
+            os << " ";
+    }
+    os << "]";
+    return os;
+}
 
 void EuclideanVector::changefirst() {
     mag_vec[0] = 20;
--- EuclideanVector.h
+++ EuclideanVector.h
@@ -12,6 +12,7 @@
 #include <iterator>
 #include <cmath>
 #include <initializer_list>
+#include <assert.h>
 
 namespace evec {
     class EuclideanVector {
@@ -33,8 +34,28 @@ namespace evec {
         EuclideanVector& operator=(const EuclideanVector&);
         EuclideanVector& operator=(EuclideanVector&&);
         unsigned int getNumDimensions() const;
-        double get(unsigned int) const;
+        double get(int) const;
         double getEuclideanNorm() const;
+        EuclideanVector createUnitVector() const;
+        double& operator[](int); // for setting via []
+        double operator[](int) const;
+        EuclideanVector& operator+=(const EuclideanVector&);
+        EuclideanVector& operator-=(const EuclideanVector&);
+        EuclideanVector& operator*=(double);
+        EuclideanVector& operator/=(double);
+        operator std::vector<double>() const;
+        operator std::list<double>() const;
+
+        //Friends
+        friend bool operator==(const EuclideanVector&, const EuclideanVector&);
+        friend bool operator!=(const EuclideanVector&, const EuclideanVector&);
+        friend EuclideanVector operator+(const EuclideanVector&, const EuclideanVector&);
+        friend EuclideanVector operator-(const EuclideanVector&, const EuclideanVector&);
+        friend double operator*(const EuclideanVector&, const EuclideanVector&);
+        friend EuclideanVector operator*(const EuclideanVector&, double);
+        friend EuclideanVector operator*(double, const EuclideanVector&);
+        friend EuclideanVector operator/(const EuclideanVector&, double);
+        friend std::ostream& operator<<(std::ostream&, const EuclideanVector&);
 
 
 
@@ -47,6 +68,15 @@ namespace evec {
         double *mag_vec = new double[1];
 
     };
+    bool operator==(const EuclideanVector&, const EuclideanVector&);
+    bool operator!=(const EuclideanVector&, const EuclideanVector&);
+    EuclideanVector operator+(const EuclideanVector&, const EuclideanVector&);
+    EuclideanVector operator-(const EuclideanVector&, const EuclideanVector&);
+    double operator*(const EuclideanVector&, const EuclideanVector&);
+    EuclideanVector operator*(const EuclideanVector&, double);
+    EuclideanVector operator*(double, const EuclideanVector&);
+    EuclideanVector operator/(const EuclideanVector&, double);
+    std::ostream& operator<<(std::ostream&, const EuclideanVector&);
 }
 
 #endif //EUCLIDEAN_VECTOR_CLASS_LIBRARY_EUCLIDEANVECTOR_H
